750c781d43e6af7ee274a8eae9c9ae9d4e63ece0
flaqo <80029050+flaqo@users.noreply.github.com>
2021-03-24 00:03:32 -0500

    Add files via upload

--- p06/Palindrome.java
+++ p06/Palindrome.java
@@ -1,4 +1,27 @@
 
 public class Palindrome {
+	
+	public static​ String mirrorA(​char​ start) throws IllegalArgumentException {
+		if (start < 65 || start > 90) {
+			throw new IllegalArgumentException("The input must be a capital char!");
+		}
+		if (start == 'A') {
+			return "A";
+		}
+		return start + " " + mirrorA((char)(start-1)) + " " + start;
+	}
+	
+	public static​ String mirrorA(​char​ start, ​int​ step) throwsIllegalArgumentException {
+		
+	}
+	
+	public static ​String mirrorZ(​char​ start) throws IllegalArgumentException {
+		
+	}
+	
+	public static ​String mirrorZ(​char​ start, ​int​ step) throwsIllegalArgumentException {
+		
+	}
 
 }
+
--- p06/PalindromeTester.java
+++ p06/PalindromeTester.java
@@ -1,5 +1,9 @@
 
 public class PalindromeTester {
+	public static boolean​ testMirrorA();
+	public static boolean​ testMirrorAStep();
+	public static boolean​ testMirrorZ();
+	public static boolean​ testMirrorZStep();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
