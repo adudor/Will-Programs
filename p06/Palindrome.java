8bcebb929e405fe4359b5e7a5aebfb3c0b330c77
flaqo <80029050+flaqo@users.noreply.github.com>
2021-03-25 01:21:50 -0500

    Add files via upload

--- p06/Palindrome.java
+++ p06/Palindrome.java
@@ -1,63 +1,64 @@
+
 public class Palindrome {
 
-  public static String mirrorA(char start) throws IllegalArgumentException {
-    if (start < 65 || start > 90) {
-      throw new IllegalArgumentException("The input must be a capital char!");
-    }
-    String returnString = "" + start;
-    if (start > 'A') {
-      return returnString + " " + mirrorA((char) (start - 1)) + " " + returnString;
-    } else {
-      return "A";
-    }
-  }
+	public static String mirrorA(char start) throws IllegalArgumentException {
+		if (start < 65 || start > 90) {
+			throw new IllegalArgumentException("The input must be a capital char!");
+		}
+		String returnString = "" + start;
+		if (start > 'A') {
+			return returnString + " " + mirrorA((char) (start - 1)) + " " + returnString;
+		} else {
+			return "A";
+		}
+	}
 
-  public static String mirrorA(char start, int step) throws IllegalArgumentException {
-    if (start < 65 || start > 90) {
-      throw new IllegalArgumentException("The input must be a capital char!");
-    }
-    String returnString = "" + start;
-    if (start > 'A') {
-      if (start - step >= 'A') {
-        return returnString + " " + mirrorA((char) (start - step), step) + " " + returnString;
-      }
-    }
-    if (start == 'A') {
-      return "A";
-    } else {
-      return returnString + " " + returnString;
-    }
+	public static String mirrorA(char start, int step) throws IllegalArgumentException {
+		if ((start < 65 || start > 90) && step > 0) {
+			throw new IllegalArgumentException("The input must be a capital char!");
+		}
+		String returnString = "" + start;
+		if (start > 'A') {
+			if (start - step >= 'A') {
+				return returnString + " " + mirrorA((char) (start - step), step) + " " + returnString;
+			}
+		}
+		if (start == 'A') {
+			return "A";
+		} else {
+			return returnString + " " + returnString;
+		}
 
-  }
+	}
 
-  public static String mirrorZ(char start) throws IllegalArgumentException {
-    if (start < 65 || start > 90) {
-      throw new IllegalArgumentException("The input must be a capital char!");
-    }
-    String returnString = "" + start;
-    if (start < 'Z') {
-      return returnString + " " + mirrorZ((char) (start + 1)) + " " + returnString;
-    } else {
-      return "Z";
-    }
-  }
+	public static String mirrorZ(char start) throws IllegalArgumentException {
+		if (start < 65 || start > 90) {
+			throw new IllegalArgumentException("The input must be a capital char!");
+		}
+		String returnString = "" + start;
+		if (start < 'Z') {
+			return returnString + " " + mirrorZ((char) (start + 1)) + " " + returnString;
+		} else {
+			return "Z";
+		}
+	}
 
-  public static String mirrorZ(char start, int step) throws IllegalArgumentException {
-    if (start < 65 || start > 90) {
-      throw new IllegalArgumentException("The input must be a capital char!");
-    }
-    String returnString = "" + start;
-    if (start < 'Z') {
-      if (start + step <= 'Z') {
-        return returnString + " " + mirrorZ((char) (start + step), step) + " " + returnString;
-      }
-    }
-    if (start == 'Z') {
-      return "Z";
-    } else {
-      return returnString + " " + returnString;
-    }
+	public static String mirrorZ(char start, int step) throws IllegalArgumentException {
+		if ((start < 65 || start > 90) && step > 0) {
+			throw new IllegalArgumentException("The input must be a capital char!");
+		}
+		String returnString = "" + start;
+		if (start < 'Z') {
+			if (start + step <= 'Z') {
+				return returnString + " " + mirrorZ((char) (start + step), step) + " " + returnString;
+			}
+		}
+		if (start == 'Z') {
+			return "Z";
+		} else {
+			return returnString + " " + returnString;
+		}
 
-  }
+	}
 
 }
--- p06/PalindromeTester.java
+++ p06/PalindromeTester.java
@@ -1,12 +1,175 @@
 
 public class PalindromeTester {
-	public static boolean​ testMirrorA();
-	public static boolean​ testMirrorAStep();
-	public static boolean​ testMirrorZ();
-	public static boolean​ testMirrorZStep();
+	public static boolean testMirrorA() {
+		{
+			try {
+				if (!Palindrome.mirrorA('E').equals("E D C B A B C D E")) {
+					return false;
+				}
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+
+		}
+
+		{
+			try {
+				if (Palindrome.mirrorA('&').equals("E D C B A B C D E")) {
+					return false;
+				}
+				return false;
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+		}
+
+		{
+			try {
+				if (Palindrome.mirrorA('a').equals("E D C B A B C D E")) {
+					return false;
+				}
+				return false;
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static boolean testMirrorAStep() {
+		{
+			try {
+				if (!Palindrome.mirrorA('E', 2).equals("E C A C E")) {
+					return false;
+				}
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+		}
+
+		{
+			try {
+				if (Palindrome.mirrorA('&', 2).equals("E C A C E")) {
+					return false;
+				}
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+		}
+
+		{
+			try {
+				if (Palindrome.mirrorA('a', 2).equals("E C A C E")) {
+					return false;
+				}
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public static boolean testMirrorZ() {
+		{
+			try {
+				if (!Palindrome.mirrorZ('V').equals("V W X Y Z Y X W V")) {
+					return false;
+				}
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+		}
+
+		{
+			try {
+				if (Palindrome.mirrorZ('&').equals("V W X Y Z Y X W V")) {
+					return false;
+				}
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+		}
+
+		{
+			try {
+				if (Palindrome.mirrorZ('a').equals("V W X Y Z Y X W V")) {
+					return false;
+				}
+				return false;
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static boolean testMirrorZStep() {
+		{
+			try {
+				if (!Palindrome.mirrorZ('V', 2).equals("V X Z X V")) {
+					return false;
+				}
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+		}
+
+		{
+			try {
+				if (Palindrome.mirrorZ('&', 2).equals("V X Z X V")) {
+					return false;
+				}
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+					return false;
+				}
+			}
+		}
+
+		{
+			try {
+				if (Palindrome.mirrorZ('a', 2).equals("V X Z X V")) {
+					return false;
+				}
+			} catch (IllegalArgumentException e) {
+				if (!e.getMessage().equals("The input must be a capital char!")) {
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static boolean runAllTests() {
+		System.out.println("testMirrorA(): " + testMirrorA());
+		System.out.println("testMirrorAStep(): " + testMirrorAStep());
+		System.out.println("testMirrorZ(): " + testMirrorZ());
+		System.out.println("testMirrorZStep(): " +testMirrorZStep());
+		return true;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(runAllTests());
 
 	}
 
