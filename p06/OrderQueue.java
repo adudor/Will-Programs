28e40c8170667e7c61f46572f23caaca1b5dd8c7
flaqo <80029050+flaqo@users.noreply.github.com>
2021-04-15 15:22:45 -0500

    Add files via upload

--- p06/OrderQueue.java
+++ p06/OrderQueue.java
@@ -1,24 +1,30 @@
 import java.util.Iterator;
 
-public class OrderQueue implements QueueADT<Order> , java.lang.Iterable<Order> {
-private LinkedOrder front;
-private LinkedOrder back;
-private LinkedOrder size;
+public class OrderQueue implements QueueADT<Order>, java.lang.Iterable<Order> {
+	private LinkedOrder front;
+	private LinkedOrder back;
+	private LinkedOrder size;
+
 	@Override
 	public Iterator<Order> iterator() {
 		// TODO Auto-generated method stub
-		return null;
+		return new OrderIterator(front);
+		
 	}
 
 	@Override
 	public void enqueue(Order newElement) {
 		// TODO Auto-generated method stub
+		back.setNext(newElement);
+		size++;
 		
+
 	}
 
 	@Override
 	public Order dequeue() {
 		// TODO Auto-generated method stub
+		newElement.remove(front);
 		return null;
 	}
 
@@ -31,7 +37,30 @@ private LinkedOrder size;
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		if (OrderQueue != null) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
+	/**
+	 * Creates and returns a String representation of this OrderQueue using an
+	 * enhanced-for loop. For example, a queue with three Orders might look like
+	 * this: 
+	 * 1001: fries (2) -> 1002: shake (1) -> 1003: burger (3) -> END
+	 * 
+	 * @return A String representation of the queue
+	 */
+	@Override
+	public String toString() {
+		if (this.size == 0) return "";
+		String qString = "";
+		for (Order o : this) {
+			qString += o.toString();
+			qString += " -> ";
+		}
+		qString += "END";
+		return qString;
+	}
 }
